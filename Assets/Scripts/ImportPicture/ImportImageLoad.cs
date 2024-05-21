f4919f77eed875d6a14592631278f1282aa204eb
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2024-05-21 13:35:02 -0400

    asdf
    asdf

---
+++ Assets/Scripts/ImportPicture/ImportImageLoad.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImportImageLoad : MonoBehaviour
+{
+    string path;
+    private Sprite convertedSprite;
+    private Texture2D actualTexture;
+    public SpriteRenderer sr;
+    public SaveFile saveSystem;
+
+    public GameObject prefabGuppy;
+
+    public void loadFish()
+    {
+        
+    }
+}
