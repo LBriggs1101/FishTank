58ddecc022851087d95e6535dd3afa1b59a679c1
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2024-09-30 05:46:39 -0400

    framework for saving class periods
    asdf

---
+++ Assets/Scripts/Save/SaveClassPeriod.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveClassPeriod : MonoBehaviour
+{
+
+    public ClassPeriodManager classPeriodManager;
+    
+    //Method to append a save file (1-7) with a fish name
+
+    //Method to load that save file (1-7) every line is a fish name
+
+    //Method to overwrite a different save file (1-7) with the new background
+
+    //Method to load that different save file background
+
+    //Method to delete a specified class period
+
+    //If possible delete specific fish in a class period but not likely, at least for this version. 
+}
---
+++ Assets/Scripts/ScriptableObject/ClassPeriodManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ClassPeriodManager", menuName = "ClassPeriodManager", order = 0)]
+public class ClassPeriodManager : ScriptableObject 
+{
+    public int ClassPeriod;
+}
